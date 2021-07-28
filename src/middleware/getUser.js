44cafd144290d58f2e87de57e23c13d7a1757391
sudo-vaibhav <mailvaibhavchopra@gmail.com>
2021-07-28 17:56:37 +0530

    moved route up

--- src/middleware/getUser.js
+++ src/middleware/getUser.js
@@ -1,32 +1,30 @@
-const participantModel = require('../models/Participant')
-const Organiser = require('../models/Organiser')
-const errorHandler = require('./errorHandler')
-const { NotFoundError, BadRequestError } = require('../utils/error')
+const participantModel = require("../models/Participant");
+const Organiser = require("../models/Organiser");
+const errorHandler = require("./errorHandler");
+const { NotFoundError, BadRequestError } = require("../utils/error");
 
-const getParticipant = async (req,res,next)=>{
-    try {
-        console.log('hi from GP')
-        const participant = await participantModel.findOne({uid:req.userId})
-        if(!participant){
-            return errorHandler(new NotFoundError,req,res)
-        }
-    req.participant = participant
-    next()
-    } catch (e) {
-        console.log(e)
-         errorHandler(new BadRequestError,req,res)
+const getParticipant = async (req, res, next) => {
+  console.log("hi from GP");
+  try {
+    const participant = await participantModel.findOne({ uid: req.userId });
+    if (!participant) {
+      return errorHandler(new NotFoundError(), req, res);
     }
-    
-}
+    req.participant = participant;
+    return next();
+  } catch (e) {
+    console.log(e);
+    errorHandler(new BadRequestError(), req, res);
+  }
+};
 
-const getOrganiser = (async (req,res,next)=>{
-    const organiser = await Organiser.findOne({uid:req.userId})
-    if(!organiser){
-        return res.status(404).send('no profile found!')
-    }
-    req.organiser = organiser
-    next()
-})
+const getOrganiser = async (req, res, next) => {
+  const organiser = await Organiser.findOne({ uid: req.userId });
+  if (!organiser) {
+    return res.status(404).send("no profile found!");
+  }
+  req.organiser = organiser;
+  next();
+};
 
-
-module.exports = {getParticipant,getOrganiser}
+module.exports = { getParticipant, getOrganiser };
