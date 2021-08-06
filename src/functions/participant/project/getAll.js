b77a1996867bd3c0b60e90ab1677570f2574c81d
Deep Gandhi <deepgandhi151@gmail.com>
2021-08-06 22:31:07 +0530

    get all projects

--- src/functions/participant/project/getAll.js
+++ src/functions/participant/project/getAll.js
@@ -5,11 +5,18 @@ const { NotFoundError, BadRequestError } = require('../../../utils/error')
 
 const getAll = async(req,res)=>{
     try {
-        const individualProjects = await projectModel.find({participant_id:req.participant._id})
-        const teams = await DN_Team.find({'members.uid':req.participant._id,project_name:{$exists:true},project_description:{$exists:true}})
+        let individualProjects = []
+        let teams = []
+        if(req.query.participant_id){
+            individualProjects = await projectModel.find({participant_id:req.query.participant_id})
+            teams = await DN_Team.find({'members.uid':req.participant._id,project_name:{$exists:true},project_description:{$exists:true}})
+        }
+        else{
+            individualProjects = await projectModel.find({participant_id:req.participant._id})
+        teams = await DN_Team.find({'members.uid':req.participant._id,project_name:{$exists:true},project_description:{$exists:true}})
+        }
         if((!teams || teams.length==0) && (!individualProjects || individualProjects.length==0)){
-            errorHandler(new NotFoundError,req,res)
-            return
+            return errorHandler(new NotFoundError,req,res)
         }
         console.log(individualProjects,teams)
         res.status(200).send({individualProjects,teams})
