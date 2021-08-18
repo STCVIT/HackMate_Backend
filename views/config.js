5823769453092acc55af2ae4b4b4e09ebc4cd0f2
deepg7 <deepgandhi151@gmail.com>
2021-08-18 18:42:27 +0000

    node version changed

--- views/config.js
+++ views/config.js
@@ -1,12 +1,11 @@
- // Your web app's Firebase configuration
- var firebaseConfig = {
-    apiKey: "AIzaSyBoUb0hvHl_U8gRkz8ZZkrSSQSvxvQdAQM",
-    authDomain: "hackportal-450d0.firebaseapp.com",
-    projectId: "hackportal-450d0",
-    storageBucket: "hackportal-450d0.appspot.com",
-    messagingSenderId: "770962135087",
-    appId: "1:770962135087:web:3904056387ded9b7420eda"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
- 
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBoUb0hvHl_U8gRkz8ZZkrSSQSvxvQdAQM",
+  authDomain: "hackportal-450d0.firebaseapp.com",
+  projectId: "hackportal-450d0",
+  storageBucket: "hackportal-450d0.appspot.com",
+  messagingSenderId: "770962135087",
+  appId: "1:770962135087:web:3904056387ded9b7420eda",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
