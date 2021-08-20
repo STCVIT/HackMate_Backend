b26b666c908d1d6af066c813e0c837c989f827f7
deepg7 <deepgandhi151@gmail.com>
2021-08-20 08:05:02 +0000

    get team by name changed

--- src/functions/participant/DN_team/get_team_by_name.js
+++ src/functions/participant/DN_team/get_team_by_name.js
@@ -36,7 +36,7 @@ const get_team_by_name = async(req,res)=>{
         }
         else{
             console.log('hi')
-            const teams = await DN_Team.find({hack_id:null,name:req.query.name}).collation({locale:'en', strength:2})
+            const teams = await DN_Team.find({hack_id:null,name:req.query.name,'members.uid':{$ne:req.participant._id}}).collation({locale:'en', strength:2})
             if(!teams || teams.length==0){
                 return errorHandler(new NotFoundError,req,res)
             }
@@ -64,4 +64,67 @@ const get_team_by_name = async(req,res)=>{
     
 } 
 
+const get_team = async(req,res)=>{
+    try {
+        if(req.query.name==undefined){
+            return res.status(400).send('please enter a name')
+        }
+        let final = []
+        let i = 0
+        let teams = []
+        if(req.params.hack_id != 'null'){
+            teams = await DN_Team.find({hack_id:req.params.hack_id,name:req.query.name,'members.uid':{$ne:req.participant._id}}).collation({locale:'en', strength:2})
+        }
+        else{
+            teams = await DN_Team.find({hack_id:null,name:req.query.name,'members.uid':{$ne:req.participant._id}}).collation({locale:'en', strength:2})    
+        }
+        if(!teams || teams.length==0){
+            return errorHandler(new NotFoundError,req,res)
+        }
+        teams.forEach(async(team)=>{
+            const skills = await SkillVacancy.find({team_id:team._id})
+            const members = team.members.map((member)=>member.uid)
+            const participants = await Participant.find({_id:{$in:members}})
+            const temp = {
+                team,
+                participants,
+                skills
+            }
+            final.push(temp)
+            i++
+            if(teams.length==i){
+                return res.status(200).send(final)
+            }
+        })
+        // await Promise.all(teams.map(async(team)=>{
+        //     const skills = await SkillVacancy.find({team_id:team._id})
+        //     const members = team.members.map((member)=>member.uid)
+        //     const participants = await Participant.find({_id:{$in:members}})
+        //     const temp = {
+        //         team,
+        //         participants,
+        //         skills
+        //     }
+        //     final.push(temp)
+        // }))
+        // return res.status(200).send(final)        
+    } catch (e) {
+        errorHandler(new BadRequestError,req,res)
+    }
+}
+
+
 module.exports = get_team_by_name
+
+await Promise.all(teams.map(async(team)=>{
+    const skills = await SkillVacancy.find({team_id:team._id})
+    const members = team.members.map((member)=>member.uid)
+    const participants = await Participant.find({_id:{$in:members}})
+    const temp = {
+        team,
+        participants,
+        skills
+    }
+    final.push(temp)
+}))
+return res.status(200).send(final)
