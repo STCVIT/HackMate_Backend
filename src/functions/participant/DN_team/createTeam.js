a963176695e798245c5e5aa07da7ac3dbce9b8b1
deepg7 <deepgandhi151@gmail.com>
2021-09-24 11:07:13 +0000

    team name error resolved

--- src/functions/participant/DN_team/createTeam.js
+++ src/functions/participant/DN_team/createTeam.js
@@ -1,34 +1,36 @@
-require('../../../db/mongoose')
-const errorHandler = require('../../../middleware/errorHandler')
-const teamCode = require('../../../middleware/teamCode')
-const DN_Team = require('../../../models/Dn-Team')
-const { DuplicateTeamHackError } = require('../../../utils/error')
+require("../../../db/mongoose");
+const errorHandler = require("../../../middleware/errorHandler");
+const teamCode = require("../../../middleware/teamCode");
+const DN_Team = require("../../../models/Dn-Team");
+const {
+  DuplicateTeamHackError,
+  DuplicateTeamNameError,
+} = require("../../../utils/error");
 
-const createTeam = async(req,res) =>{
-    try { 
-            const team_code = teamCode()
-            const team = new DN_Team({
-                name: req.body.name,
-                admin_id:req.participant._id,
-                team_code,
-                members:{uid:req.participant._id}
-            })
-            if(req.params.hack_id != 'null'){
-                team.hack_id = req.params.hack_id
-            }
-            await team.save()
-            res.status(201).send(team)
-        }catch(e){
-            if(e.code===11000){
-                return errorHandler(new DuplicateTeamHackError,req,res)
-            }
-            else{
-                return errorHandler(e,req,res)
-            }
-            
-        }
-}
+const createTeam = async (req, res) => {
+  try {
+    if (await DN_Team.findOne({ name: req.body.name })) {
+      return errorHandler(new DuplicateTeamNameError(), req, res);
+    }
+    const team_code = teamCode();
+    const team = new DN_Team({
+      name: req.body.name,
+      admin_id: req.participant._id,
+      team_code,
+      members: { uid: req.participant._id },
+    });
+    if (req.params.hack_id != "null") {
+      team.hack_id = req.params.hack_id;
+    }
+    await team.save();
+    res.status(201).send(team);
+  } catch (e) {
+    if (e.code === 11000) {
+      return errorHandler(new DuplicateTeamHackError(), req, res);
+    } else {
+      return errorHandler(e, req, res);
+    }
+  }
+};
 
-
-
-module.exports = createTeam
+module.exports = createTeam;
--- src/utils/error.js
+++ src/utils/error.js
@@ -1,110 +1,119 @@
-class NotFoundError extends Error{
-    constructor(){
-        super()
-        this.statusCode = 404
-        this.message = 'Not Found'
-    }
+class NotFoundError extends Error {
+  constructor() {
+    super();
+    this.statusCode = 404;
+    this.message = "Not Found";
+  }
 }
 
-class AuthenticationError extends Error{
-    constructor(){
-        super()
-        this.statusCode = 401
-        this.message = 'Not Authenticated'
-    }
+class AuthenticationError extends Error {
+  constructor() {
+    super();
+    this.statusCode = 401;
+    this.message = "Not Authenticated";
+  }
 }
 
-class BadRequestError extends Error{
-    constructor(){
-        super()
-        this.statusCode = 400
-        this.message = 'Bad Request'
-    }
+class BadRequestError extends Error {
+  constructor() {
+    super();
+    this.statusCode = 400;
+    this.message = "Bad Request";
+  }
 }
 
-class TeamFullError extends Error{
-    constructor(){
-        super()
-        this.statusCode = 403
-        this.message = 'Team is Full'
-    }
+class TeamFullError extends Error {
+  constructor() {
+    super();
+    this.statusCode = 403;
+    this.message = "Team is Full";
+  }
 }
 
-class DuplicateTeamEntryError extends Error{
-    constructor(){
-        super()
-        this.statusCode = 403
-        this.message = 'Duplicate Entry in Team is not allowed'
-    }
+class DuplicateTeamEntryError extends Error {
+  constructor() {
+    super();
+    this.statusCode = 403;
+    this.message = "Duplicate Entry in Team is not allowed";
+  }
 }
 
-class EmailUnauthorizedError extends Error{
-    constructor(){
-        super()
-        this.statusCode = 401
-        this.message = 'Please Verify Your Email Address'
-    }
+class EmailUnauthorizedError extends Error {
+  constructor() {
+    super();
+    this.statusCode = 401;
+    this.message = "Please Verify Your Email Address";
+  }
 }
 
-class DuplicateTeamHackError extends Error{
-    constructor(){
-        super()
-        this.statusCode = 403
-        this.message = 'Already Going to the same Hack.'
-    }
+class DuplicateTeamHackError extends Error {
+  constructor() {
+    super();
+    this.statusCode = 403;
+    this.message = "Already Going to the same Hack.";
+  }
 }
 
-class SchemaValidationError extends Error{
-    constructor(){
-        super()
-        this.statusCode = 400
-        this.message = 'Please Specify All required fields'
-    }
+class SchemaValidationError extends Error {
+  constructor() {
+    super();
+    this.statusCode = 400;
+    this.message = "Please Specify All required fields";
+  }
 }
 
-class DuplicateEntryError extends Error{
-    constructor(){
-        super()
-        this.statusCode = 409
-        this.message = 'Indexing error'
-    }
+class DuplicateEntryError extends Error {
+  constructor() {
+    super();
+    this.statusCode = 409;
+    this.message = "Indexing error";
+  }
 }
 
-class InvalidUpdatesError extends Error{
-    constructor(){
-        super()
-        this.statusCode = 400
-        this.message = 'Invalid Updates'
-    }
+class InvalidUpdatesError extends Error {
+  constructor() {
+    super();
+    this.statusCode = 400;
+    this.message = "Invalid Updates";
+  }
 }
 
-class ClaimNotSetError extends Error{
-    constructor(){
-        super()
-        this.statusCode = 418
-        this.message = 'Claim Not Set'
-    }
+class ClaimNotSetError extends Error {
+  constructor() {
+    super();
+    this.statusCode = 418;
+    this.message = "Claim Not Set";
+  }
 }
 
-class LoginNotAllowedError extends Error{
-    constructor(){
-        super()
-        this.statusCode = 403
-        this.message = 'Login Not Allowed'
-    }
+class LoginNotAllowedError extends Error {
+  constructor() {
+    super();
+    this.statusCode = 403;
+    this.message = "Login Not Allowed";
+  }
+}
+
+class DuplicateTeamNameError extends Error {
+  constructor() {
+    super();
+    this.statusCode = 418;
+    this.message = "Duplicate name not allowed";
+  }
 }
 
 module.exports = {
-    NotFoundError,
-    AuthenticationError,
-    BadRequestError,
-    TeamFullError,
-    DuplicateTeamEntryError,
-    EmailUnauthorizedError,
-    DuplicateTeamHackError,
-    SchemaValidationError,
-    DuplicateEntryError,
-    InvalidUpdatesError,
-    LoginNotAllowedError,
-    ClaimNotSetError
-}
+  NotFoundError,
+  AuthenticationError,
+  BadRequestError,
+  TeamFullError,
+  DuplicateTeamEntryError,
+  EmailUnauthorizedError,
+  DuplicateTeamHackError,
+  SchemaValidationError,
+  DuplicateEntryError,
+  InvalidUpdatesError,
+  LoginNotAllowedError,
+  ClaimNotSetError,
+  DuplicateTeamNameError,
+};
