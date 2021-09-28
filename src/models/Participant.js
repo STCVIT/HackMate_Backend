071acf4a4e90c5a8ffa202f2002f7999ec4854a7
deepg7 <deepgandhi151@gmail.com>
2021-09-28 19:20:40 +0000

    updated validators

--- src/models/Participant.js
+++ src/models/Participant.js
@@ -42,14 +42,6 @@ const participantSchema = new mongoose.Schema({
   },
   website: {
     type: String,
-    validate: {
-      validator: function (website) {
-        return /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/.test(
-          website
-        );
-      },
-      message: `Enter a valid link!`,
-    },
   },
   photo: {
     type: String,
