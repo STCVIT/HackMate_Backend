053d110185b96c71b7f1c527b3137fab9bc2e7ec
deepg7 <deepgandhi151@gmail.com>
2021-09-28 11:16:16 +0000

    checking for unique name in get admin teams

--- src/functions/participant/DN_team/myAdminTeams.js
+++ src/functions/participant/DN_team/myAdminTeams.js
@@ -1,62 +1,89 @@
-const DN_Team = require('../../../models/Dn-Team')
-const Hack = require('../../../models/Hack')
-const {NotFoundError, BadRequestError}  = require('../../../utils/error')
-const errorHandler = require('../../../middleware/errorHandler')
-const Skill = require('../../../models/Skill')
-const participantModel = require('../../../models/Participant')
-const myAdminTeams = async(req,res)=>{
-    try {
-    const check = await DN_Team.findOne({'members.uid':req.participant._id,hack_id:req.params.hack_id})
-    if(check){
-        return res.status(403).send('already going to hack,cannot add another team')
+const DN_Team = require("../../../models/Dn-Team");
+const Hack = require("../../../models/Hack");
+const { NotFoundError, BadRequestError } = require("../../../utils/error");
+const errorHandler = require("../../../middleware/errorHandler");
+const Skill = require("../../../models/Skill");
+const participantModel = require("../../../models/Participant");
+const myAdminTeams = async (req, res) => {
+  try {
+    const check = await DN_Team.findOne({
+      "members.uid": req.participant._id,
+      hack_id: req.params.hack_id,
+    });
+    if (check) {
+      return res
+        .status(403)
+        .send("already going to hack,cannot add another team");
     }
-        let myTeams=[]
-        const hack = await Hack.findOne({_id:req.params.hack_id})
-        const hackTeams = await DN_Team.find({hack_id:req.params.hack_id}) 
-        if(!hackTeams || hackTeams.length==0){
-             myTeams = await DN_Team.find({admin_id:req.participant._id,hack_id:null})
-           if(myTeams.length==0 || !myTeams){
-               return errorHandler(new NotFoundError,req,res)
-           }
-        }else{
-            let members = []
-            let temp = hackTeams.map((team)=>{return team.members})
-            temp.forEach(member => {
-                member.forEach((doc)=>members.push(doc.uid))
-            });
-            
-              myTeams = await DN_Team.find({'members.uid':{$nin:members},hack_id:null,admin_id:req.participant._id})
-            if(!myTeams || myTeams.length==0){
-                return errorHandler(new NotFoundError,req,res)
-            }
-        }
-        let eligibleTeams = []
-        myTeams.forEach((team)=>{
-            if(team.members.length<=hack.max_team_size){
-                eligibleTeams.push(team)
-            }
-        })
-        if(!eligibleTeams){
-           return errorHandler(new NotFoundError,req,res)
-        }
-        let final = []
-        await Promise.all(eligibleTeams.map(async(team)=>{
-            let pt_skill = []
-            let members = team.members.map((member)=>member.uid)
-            let participants = await participantModel.find({_id:{$in:members}})
-            await Promise.all(participants.map(async(participant)=>{
-                let skills = await Skill.find({participant_id:participant._id})
-                pt_skill.push({participant,skills})
-            }))
-            final.push({team,pt_skill})
-        }))
-        res.status(200).send(final)
-    } catch (e) {
-        errorHandler(new BadRequestError,req,res)
-    }
-}
+    let myTeams = [];
+    const hack = await Hack.findOne({ _id: req.params.hack_id });
+    const hackTeams = await DN_Team.find({ hack_id: req.params.hack_id });
+    if (!hackTeams || hackTeams.length == 0) {
+      myTeams = await DN_Team.find({
+        admin_id: req.participant._id,
+        hack_id: null,
+      });
+      if (myTeams.length == 0 || !myTeams) {
+        return errorHandler(new NotFoundError(), req, res);
+      }
+    } else {
+      let members = [];
+      let temp = hackTeams.map((team) => {
+        return team.members;
+      });
+      temp.forEach((member) => {
+        member.forEach((doc) => members.push(doc.uid));
+      });
 
-module.exports = myAdminTeams
+      myTeams = await DN_Team.find({
+        "members.uid": { $nin: members },
+        hack_id: null,
+        admin_id: req.participant._id,
+      });
+      if (!myTeams || myTeams.length == 0) {
+        return errorHandler(new NotFoundError(), req, res);
+      }
+    }
+    let eligibleTeams = [];
+    myTeams.forEach((team) => {
+      if (
+        team.members.length <= hack.max_team_size &&
+        !(await DN_Team.findOne({
+          name: team.name,
+          hack_id: req.params.hack_id,
+        }))
+      ) {
+        eligibleTeams.push(team);
+      }
+    });
+    if (!eligibleTeams) {
+      return errorHandler(new NotFoundError(), req, res);
+    }
+    let final = [];
+    await Promise.all(
+      eligibleTeams.map(async (team) => {
+        let pt_skill = [];
+        let members = team.members.map((member) => member.uid);
+        let participants = await participantModel.find({
+          _id: { $in: members },
+        });
+        await Promise.all(
+          participants.map(async (participant) => {
+            let skills = await Skill.find({ participant_id: participant._id });
+            pt_skill.push({ participant, skills });
+          })
+        );
+        final.push({ team, pt_skill });
+      })
+    );
+    res.status(200).send(final);
+  } catch (e) {
+    console.log(error);
+    errorHandler(new BadRequestError(), req, res);
+  }
+};
+
+module.exports = myAdminTeams;
 
 //OLD CODE
 // for await (team of eligibleTeams){
@@ -70,4 +97,3 @@ module.exports = myAdminTeams
 //     final.push({team,pt_skill})
 // }
 // res.status(200).send(final)
-
